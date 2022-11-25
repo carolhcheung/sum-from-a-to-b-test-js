5cbfd924e35f5d2e9f02a9fdd71f725431c52391
Carol Cheung <carol.cheung@telus.com>
2022-11-24 22:08:07 -0800

    sum from a to b test added

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-
 function sum(fromN, toN) {
   // Sum all the values from fromN up to toN
+  let result = 0;
+    for(var i = fromN; i <= toN; i++) {
+      result += i;
+    }
+  return result;
+  // accumulator to count ++
 }
 
 module.exports = sum;
